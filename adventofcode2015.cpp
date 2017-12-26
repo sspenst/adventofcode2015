680633d0e00321e34ae014ed86af005f7320eb5d
sspenst <spencerspenst@gmail.com>
2017-12-25 22:35:35 -0800

    added an array of functions corresponding to each day's solution

--- adventofcode2015.cpp
+++ adventofcode2015.cpp
@@ -2,8 +2,15 @@
 
 #include "src/days.h"
 
+day_fn day_fns[] = { day01 };
+
 int main(int argc, char* argv[])
 {
-	day01();
+	// set the day to be executed
+	int day = 1;
+	if (argc == 2) day = atoi(argv[1]);
+
+	// call the corresponding day
+	day_fns[day - 1]();
 	return 0;
 }
--- src/days.h
+++ src/days.h
@@ -3,4 +3,6 @@
 
 using namespace std;
 
+typedef void(*day_fn)(void);
+
 void day01();
