8d61eea83770e391177ce9a5d80547bb18fd4eef
sspenst <spencerspenst@gmail.com>
2017-12-25 18:19:44 -0800

    day 1 solutions

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,5 +1,23 @@
 #include "days.h"
 
 void day01() {
-	cout << "hello" << endl;
+	ifstream infile("input/day01.in");
+	string text{ istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
+
+	int floor = 0;
+	bool increment_pos = true;
+	int pos = 0;
+
+	for (char & c : text) {
+		// part 1: floor counting
+		if (c == '(') floor += 1;
+		else if (c == ')') floor -= 1;
+		
+		// part 2: increment until total falls below 0
+		if (increment_pos) pos += 1;
+		if (floor < 0) increment_pos = false;
+	}
+
+	cout << "Part 1: " << floor << endl;
+	cout << "Part 2: " << pos << endl;
 }
